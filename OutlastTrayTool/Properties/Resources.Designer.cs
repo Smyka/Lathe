f2ece870dccc8e3f6b0e07543d4ea6a5c4dcd061
Smyka <69325320+Smyka@users.noreply.github.com>
2026-04-19 21:41:23 -0500

    rename to Lathe

--- OutlastTrayTool/Properties/Resources.Designer.cs
+++ OutlastTrayTool/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace OutlastTrayTool.Properties {
+namespace Lathe.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace OutlastTrayTool.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("OutlastTrayTool.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Lathe.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
